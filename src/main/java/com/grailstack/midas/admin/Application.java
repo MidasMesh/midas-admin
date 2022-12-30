85964f36135acb425a47fcc20776e90307a4769c
itrickzhang <625948212@qq.com>
2022-12-30 09:21:35 +0800

    [imporve]swagger升级至3.0.0 #5

--- src/main/java/com/grailstack/midas/admin/Application.java
+++ src/main/java/com/grailstack/midas/admin/Application.java
@@ -10,8 +10,8 @@ import org.xujin.halo.annotation.domain.Domain;
  * @author halo codegen
  */
 @SpringBootApplication
-@HaloApplication
-@Domain(code = "basic", parentCode = "bp", name = "基础域", desc = "基础服务域")
+@HaloApplication(appId = "midas-admin",appName = "midas-admin")
+@Domain(code="midas-admin",name = "管控端")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
