ebba2b77fe857369433d4b818cc879b562f99691
itrickzhang <625948212@qq.com>
2022-12-30 09:37:30 +0800

    [imporve]重构管控端工程结构 #7

---
+++ midas-admin-integration/src/main/java/com/grailstack/midas/Main.java
@@ -0,0 +1,12 @@
+package com.grailstack.midas;
+
+/**
+ * @author zcb
+ * @description: TODO
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
